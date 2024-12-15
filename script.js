a5df254c6c85a01e3d393ac641646da0344858df
Isacksales <isacsales02@gmail.com>
2024-12-14 21:11:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ function showMessage(resposta) {
   let message;
   switch (resposta) {
     case 'Sim':
-      message = 'Estou muito feliz por ter aceitado, saiba que lutarei por nós sempre, vou fazer valer a pena esse sim, te amo muito';
+      message = 'Estou muito feliz por ter aceitado, saiba que lutarei por nós sempre, vou fazer valer a pena esse sim, te amo muito.';
       setTimeout(() => {
         window.location.href = "memorias.html"; // Redireciona para a página de memórias
       }, 10000); // 10000 milissegundos = 10 segundos
       break;
     case 'Não':
-      message = 'Que pena, mas tudo bem, talvez ainda não seja o momento, mas saiba que te amo muito e, se você permitir, irei tentar te conquistar mais a cada dia, até que aceite';
+      message = 'Que pena, mas tudo bem, talvez ainda não seja o momento, mas saiba que te amo muito e, se você permitir, irei tentar te conquistar mais a cada dia, até que aceite.';
       break;
   }
 
